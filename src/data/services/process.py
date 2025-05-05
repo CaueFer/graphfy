a809d1e90a99dd9daf34d1148d35af911c736a2c
cauefer <cauefernandescaetano@gmail.com>
2025-05-05 15:02:14 -0300

    `refac`: Refactore estrutura de arquivos

--- src/data/services/process.py
+++ src/data/services/process.py
@@ -1,5 +1,4 @@
 from dotenv import load_dotenv
-from tortoise import Tortoise
 from pathlib import Path
 import os
 import json
--- src/server.py
+++ src/server.py
@@ -26,4 +26,4 @@ register_tortoise(
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("run:app", host="0.0.0.0", port=5000, reload=True)
+    uvicorn.run("server:app", host="0.0.0.0", port=5000, reload=True)
